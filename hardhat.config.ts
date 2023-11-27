8bc7859d614e51283f39242f28c07a734fd4aca1
arshan <arshankhanifar@gmail.com>
2023-11-27 13:28:30 -0500

    unfinished

---
+++ hardhat.config.ts
@@ -0,0 +1,12 @@
+import { HardhatUserConfig } from "hardhat/types";
+import { task } from "hardhat/config";
+
+export const config: HardhatUserConfig = {
+
+
+
+};
+
+task("hello", "says hi", (taskArgs, hre) => {
+  console.log(`Hello, ${taskArgs.name}`);
+}).addOptionalParam("name");
